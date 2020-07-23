b40567bd677c9a85c0ae1f73a5004e3fcd3e2b0f
Jacob Fromm <jacobfromm@gmail.com>
2020-07-23 15:42:37 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
- for i in array 
-  if array[i] == value_to_find
-    array[value_to_find]
-  end  
- end
+  counter = 0 
+  while counter < array.length do
+    if array[counter] == value_to_find
+      return array[value_to_find]
+    end
+  end
+  return nil
 end
